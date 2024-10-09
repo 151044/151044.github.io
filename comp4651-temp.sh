2a35abf6b91ac7dffe30d57f093ae8189b0e06f4
151044 <51814503+151044@users.noreply.github.com>
2024-10-09 16:30:39 +0800

    fix: Missing autoconfirm for comp4651-temp.sh

--- comp4651-temp.sh
+++ comp4651-temp.sh
@@ -1,4 +1,4 @@
 sudo apt update
-sudo apt install sysbench
+sudo apt install sysbench -y
 sysbench --test=cpu --num-threads=4 --cpu-max-prime=10000 run
 sysbench --test=memory --num-threads=4 --memory-total-size=2G --memory-oper=write --memory-scope=global run
