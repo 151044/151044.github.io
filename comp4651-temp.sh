4fc61b2657dd426746f337e9968dfcf48fed0785
151044 <51814503+151044@users.noreply.github.com>
2024-10-09 16:18:20 +0800

    feat: Temp file for COMP 4651

---
+++ comp4651-temp.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install sysbench
+sysbench --test=cpu --num-threads=4 --cpu-max-prime=10000 run
+sysbench --test=memory --num-threads=4 --memory-total-size=2G --memory-oper=write --memory-scope=global run
